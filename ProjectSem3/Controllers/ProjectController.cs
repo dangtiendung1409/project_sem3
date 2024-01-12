347e6704f6623bf0655c86b0f6c9c007c3b73030
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2024-01-12 14:00:24 +0700

    code

---
+++ ProjectSem3/Controllers/ProjectController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectSem3.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
